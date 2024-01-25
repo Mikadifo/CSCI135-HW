3fa099b43b21b6455cc222baebfa464bfecb1b1d
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-25 16:52:41 -0500

    add: 3.1

---
+++ integer.cpp
@@ -0,0 +1,27 @@
+/*
+Author: Michael Padilla
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: E3.1
+
+This checks if a integer is positive negative or zero
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter a integer: ";
+    cin >> number;
+
+    if (number == 0) {
+	cout << "zero" << endl;
+    } else if (number < 0) {
+	cout << "negative" << endl;
+    } else {
+	cout << "positive" << endl;
+    }
+
+    return 0;
+}
