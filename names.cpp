9345bff4b01e234f532216872f945b99207efd6a
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-25 16:21:37 -0500

    add: hw1.7

---
+++ names.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Michael Padilla
+Course: CSCI-136
+Instructor: Tong Yi
+Assignment: E1.7
+
+This program asks the user for 3 names and prints them out.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstName, secondName, thirdName;
+
+    cout << "Enter first name: ";
+    cin >> firstName;
+    cout << "Enter second name: ";
+    cin >> secondName;
+    cout << "Enter third name: ";
+    cin >> thirdName;
+
+    cout << firstName << endl << secondName << endl << thirdName << endl;
+
+    return 0;
+}
