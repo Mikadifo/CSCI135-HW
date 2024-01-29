4ed1ed243b8523564aec5571cf49fd7ceeaff16f
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-29 18:51:52 -0500

    add: lab 1 B

--- smaller3.cpp
+++ smaller3.cpp
@@ -1,6 +1,6 @@
 /*
 Author: Michael Padilla
-Course: CSCI-136
+Course: CSCI-135
 Instructor: Tong Yi
 Assignment: Lab1B
 */
@@ -12,19 +12,19 @@ It finds the smaller of 3 integers
 using namespace std;
 
 int main() {
-    int number1, number2, number3, smallest;
+  int number1, number2, number3, smallest;
 
-    cout << "Enter the first number: ";
-    cin >> number1;
-    cout << "Enter the second number: ";
-    cin >> number2;
-    cout << "Enter the third number: ";
-    cin >> number3;
+  cout << "Enter the first number: ";
+  cin >> number1;
+  cout << "Enter the second number: ";
+  cin >> number2;
+  cout << "Enter the third number: ";
+  cin >> number3;
 
-    smallest = (number1 < number2) ? number1 : number2;
-    smallest = (smallest < number3) ? smallest : number3;
+  smallest = (number1 < number2) ? number1 : number2;
+  smallest = (smallest < number3) ? smallest : number3;
 
-    cout << "The smaller of the three is " << smallest << endl;
-    
-    return 0;
+  cout << "The smaller of the three is " << smallest << endl;
+
+  return 0;
 }
