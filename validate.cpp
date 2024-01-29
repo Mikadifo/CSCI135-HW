08bb4acc0636f5f3d07de1c78c275f7b137e15f0
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-29 12:49:58 -0500

    add: lab 2 a

---
+++ validate.cpp
@@ -0,0 +1,24 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2 A
+*/
+/*
+    Asks the user for a number between 0 and 100.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    do {
+	cout << "Enter a number between 0 and 100: ";
+	cin >> number;
+    } while(number < 0 || number > 100);
+
+    cout << number*number;
+
+    return 0;
+}
