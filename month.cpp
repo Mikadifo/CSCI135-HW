0e3f20d729e1cc87f414358dbb9c803625f3e22e
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-29 18:53:40 -0500

    add: lab 1 D

--- month.cpp
+++ month.cpp
@@ -1,6 +1,6 @@
 /*
 Author: Michael Padilla
-Course: CSCI-136
+Course: CSCI-135
 Instructor: Tong Yi
 Assignment: Lab1D
 */
@@ -15,43 +15,43 @@ using namespace std;
 Returns true if a month is leap and false otherwise
 */
 bool isLeap(int year) {
-    if (year % 4 != 0) {
-	return false;
-    } else if (year % 100 != 0) {
-	return true;
-    } else if (year % 400 != 0) {
-	return false;
-    } else {
-	return true;
-    }
+  if (year % 4 != 0) {
+    return false;
+  } else if (year % 100 != 0) {
+    return true;
+  } else if (year % 400 != 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 int main() {
-    int year, month;
+  int year, month;
 
-    cout << "Enter year: ";
-    cin >> year;
-    cout << "Enter month: ";
-    cin >> month;
+  cout << "Enter year: ";
+  cin >> year;
+  cout << "Enter month: ";
+  cin >> month;
 
-    if (month == 2) {
-	cout << (isLeap(year) ? 29 : 28)  << " days";
-	return 0;
-    }
-
-    if (month <= 7) {
-	if (month % 2 != 0) {
-	    cout << 31 << " days";
-	} else {
-	    cout << 30 << " days";
-	}
-    } else {
-	if (month % 2 != 0) {
-	    cout << 30 << " days";
-	} else {
-	    cout << 31 << " days";
-	}
-    }
-
+  if (month == 2) {
+    cout << (isLeap(year) ? 29 : 28) << " days";
     return 0;
+  }
+
+  if (month <= 7) {
+    if (month % 2 != 0) {
+      cout << 31 << " days";
+    } else {
+      cout << 30 << " days";
+    }
+  } else {
+    if (month % 2 != 0) {
+      cout << 30 << " days";
+    } else {
+      cout << 31 << " days";
+    }
+  }
+
+  return 0;
 }
