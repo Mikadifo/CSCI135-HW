6db3644ccb670f7e6bab9207d53533846a92092c
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-29 16:00:08 -0500

    add: L2D

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,1 +1,31 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2 D
+*/
+/*
+ This prints a fibonacci sequence
+*/
 
+#include <iostream>
+using namespace std;
+
+/*
+ * As the numbers grow they start to be negative
+ */
+int main() {
+    int fibonacci[60];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+
+    for(int i = 2; i < 60; i++) {
+	fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+
+    for(int i = 0; i < 60; i++) {
+	cout << fibonacci[i] << endl;
+    }
+
+    return 0;
+}
