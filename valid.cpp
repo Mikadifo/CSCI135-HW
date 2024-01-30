4b0906cac64d57640abe14b47808d292ac4fecc9
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-29 19:02:36 -0500

    add: E4.8

--- valid.cpp
+++ valid.cpp
@@ -12,13 +12,13 @@ Assignment: Lab 2 A
 using namespace std;
 
 int main() {
-    int number;
-    do {
-	cout << "Enter a number between 0 and 100: ";
-	cin >> number;
-    } while(number <= 0 || number >= 100);
+  int number;
+  do {
+    cout << "Enter a number between 0 and 100: ";
+    cin >> number;
+  } while (number <= 0 || number >= 100);
 
-    cout << "Number squared is " << number*number << endl;
+  cout << "Number squared is " << number * number << endl;
 
-    return 0;
+  return 0;
 }
