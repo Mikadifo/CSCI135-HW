406cdb303b51ee9f69591a35052798de03fddd4e
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-01-30 09:18:43 -0500

    add: L2d

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -15,17 +15,17 @@ using namespace std;
  * As the numbers grow they start to be negative
  */
 int main() {
-    int fibonacci[60];
-    fibonacci[0] = 0;
-    fibonacci[1] = 1;
+  int fibonacci[60];
+  fibonacci[0] = 0;
+  fibonacci[1] = 1;
 
-    for(int i = 2; i < 60; i++) {
-	fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-    }
+  for (int i = 2; i < 60; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+  }
 
-    for(int i = 0; i < 60; i++) {
-	cout << fibonacci[i] << endl;
-    }
+  for (int i = 0; i < 60; i++) {
+    cout << fibonacci[i] << endl;
+  }
 
-    return 0;
+  return 0;
 }
