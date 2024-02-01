ec582ab8d4cc60792b9ebaf142969379fbe47eba
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-01 12:40:48 -0500

    add: Lab 4 A

---
+++ box.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 4 A
+*/
+/*
+ This program  prints a box of asterisks using a given width and height.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width, height;
+
+  cout << "Enter width: ";
+  cin >> width;
+  cout << "Enter height: ";
+  cin >> height;
+
+  cout << "Shape:" << endl;
+
+  for (int i = 0; i < height; i++) {
+    for (int i = 0; i < width; i++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
