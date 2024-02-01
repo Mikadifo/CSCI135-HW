c50dabdf6e5ec14201ff84dedbf74f56a3716281
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-01 12:44:56 -0500

    add: Lab 4 B

---
+++ checkerboard.cpp
@@ -0,0 +1,30 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 4 B
+*/
+/*
+ This program prints a checkerboard using a given width and height
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width, height;
+
+  cout << "Enter width: ";
+  cin >> width;
+  cout << "Enter height: ";
+  cin >> height;
+
+  cout << "Shape: " << endl;
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+    }
+  }
+
+  return 0;
+}
