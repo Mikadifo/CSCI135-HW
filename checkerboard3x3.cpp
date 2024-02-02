26e8300a281bc5fbf53b23c0571961eb212f9b1c
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-02 13:41:20 -0500

    add: Lab 4 G

---
+++ checkerboard3x3.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 4 G
+*/
+/*
+ This program prints a 3x3 checkerboard using the given width and height.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width, height;
+
+  cout << "Enter width: ";
+  cin >> width;
+  cout << "Enter height: ";
+  cin >> height;
+
+  return 0;
+}
