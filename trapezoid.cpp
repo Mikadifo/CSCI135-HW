2dd8531fe0a614f79a7d12c2a3083db83c15c5af
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-02 12:12:37 -0500

    add: Lab 4 F

---
+++ trapezoid.cpp
@@ -0,0 +1,40 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 4 F
+*/
+
+/*
+ This program print an upside-down trapezoid of a given width and height.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int width, height;
+  int spaces = 0;
+
+  cout << "Enter width: ";
+  cin >> width;
+  cout << "Enter height: ";
+  cin >> height;
+
+  cout << "Shape:" << endl;
+
+  if (height * 2 > width) {
+    cout << "Impossible shape!";
+    return 0;
+  }
+
+  for (int i = 0; i < height; i++) {
+    cout << std::string(spaces, ' ');
+    cout << std::string(width, '*');
+    cout << std::string(spaces, ' ') << endl;
+    width -= 2;
+    spaces += 1;
+  }
+
+  return 0;
+}
