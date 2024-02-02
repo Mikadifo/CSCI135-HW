161503c37aa0ef7059338ad80dd0d8cea5e45cd6
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-02 11:23:03 -0500

    add: Lab 4 D

---
+++ lower.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 4 D
+*/
+
+/*
+ This program prints the bottom-left half of a square using a given length.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int length;
+
+  cout << "Enter side length: ";
+  cin >> length;
+
+  for (int i = 1; i <= length; i++) {
+    cout << std::string(i, '*') << endl;
+  }
+
+  return 0;
+}
