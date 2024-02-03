b84375d1b6c5e0ae942e28d267fec7081ace7f4f
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-03 12:24:37 -0500

    add: P1C

--- calc2.cpp
+++ calc2.cpp
@@ -5,5 +5,30 @@ Instructor: Tong Yi
 Assignment: Project 1 C
 */
 /*
- This program
+ This program evaluates multiple formulas in a file separated by a semicolon
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int number, total = 0;
+  char sign;
+
+  while (cin >> number) {
+    if (sign == ';') {
+      cout << total << endl;
+      total = 0;
+    }
+    if (!sign || sign == ';') {
+      total = number;
+    } else {
+      total = (sign == '+') ? total + number : total - number;
+    }
+    cin >> sign;
+  }
+
+  cout << total << endl;
+
+  return 0;
+}
