54f91bf1349059bc6633db021e12cfb753e5ce89
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-11 12:29:37 -0500

    add: L5B

---
+++ numbers.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 5 B
+
+This program checks if a number is prime
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+  for (int i = 2; i < n - 1; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  int number;
+
+  cout << "Enter a number: ";
+  cin >> number;
+
+  cout << number << (isPrime(number) ? " is prime" : " is not prime") << endl;
+
+  return 0;
+}
