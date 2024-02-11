b0069bc0be8a76511a4786deba01b107339263bd
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-11 10:57:02 -0500

    add: E5.15

---
+++ sort3.cpp
@@ -0,0 +1,26 @@
+
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: E 5.15
+
+This program swaps the values of a, b and c until their sorted in order.
+*/
+
+using namespace std;
+
+void sort2(int &a, int &b) {
+  if (a > b) {
+    int tempA = a;
+    a = b;
+    b = tempA;
+  }
+}
+
+void sort3(int &a, int &b, int &c) {
+  sort2(b, c);
+  sort2(a, b);
+}
+
+int main() { return 0; }
