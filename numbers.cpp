fc0a6cb14f8ed0e004165e6dbfd73bf0e3ed306b
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-11 13:08:09 -0500

    add: L5C

--- numbers.cpp
+++ numbers.cpp
@@ -20,13 +20,15 @@ bool isPrime(int n) {
   return n != 1;
 }
 
+int nextPrime(int n) { return isPrime(n + 1) ? n + 1 : nextPrime(n + 1); }
+
 int main() {
   int number;
 
   cout << "Enter a number: ";
   cin >> number;
 
-  cout << number << (isPrime(number) ? " is prime" : " is not prime") << endl;
+  cout << nextPrime(number);
 
   return 0;
 }
