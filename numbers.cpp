58726afafaa368fe5bab83830028bdb1c8e4393c
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-11 12:59:14 -0500

    add: L5B

--- numbers.cpp
+++ numbers.cpp
@@ -11,13 +11,17 @@ This program checks if a number is prime
 using namespace std;
 
 bool isPrime(int n) {
+  if (n == 1 || n == 0) {
+    return false;
+  }
+
   for (int i = 2; i < n - 1; i++) {
     if (n % i == 0) {
       return false;
     }
   }
 
-  return n != 1;
+  return true;
 }
 
 int main() {
