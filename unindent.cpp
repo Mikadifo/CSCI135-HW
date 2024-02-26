634e3d63b16973669748db9df4300e0e465d6c2f
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-02-26 17:19:07 -0500

    add: L7A

---
+++ unindent.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 7 A
+
+This program reads a cpp file and removes all leading spaces from it.
+*/
+
+#include <cctype>
+#include <iostream>
+using namespace std;
+
+string removeLeadingSpaces(string line) {
+  string newLine;
+
+  for (char character : line) {
+    if (!isspace(character)) {
+      newLine = line.substr(line.find(character));
+      break;
+    }
+  }
+
+  return newLine;
+}
+
+int main() {
+  string line;
+
+  while (getline(cin, line)) {
+    cout << removeLeadingSpaces(line) << endl;
+  }
+
+  return 0;
+}
