ffabc7bc902132130a84799a2f670ad21d072f04
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-05 09:17:33 -0500

    add: Lab 9D

--- poem.cpp
+++ poem.cpp
@@ -4,6 +4,24 @@ Course: CSCI-135
 Instructor: Tong Yi
 Assignment: Lab 9 D
 
- Here, briefly, at least in one or a few sentences describe what the program
-does
+This program keeps printing poems and storing them dinamically
 */
+
+#include <string>
+using namespace std;
+
+string *createAPoemDynamically() {
+  string *p = new string;
+  *p = "Roses are red, violets are blue";
+  return p;
+}
+
+int main() {
+  while (true) {
+    string *p;
+    p = createAPoemDynamically();
+
+    // assume that the poem p is not needed at this point
+    delete p;
+  }
+}
