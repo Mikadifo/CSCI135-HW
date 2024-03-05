29a550848d3802386b85b6c1914fe93267d8b3fb
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-04 19:13:36 -0500

    add: L9A

---
+++ 3d-space.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 9 A
+
+This program receives the coordinates of a point P passed as a pointer, and
+computes the distance from the origin to the point P
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int;
+
+  return 0;
+}
