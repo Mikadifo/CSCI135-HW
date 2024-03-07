dcb4c8e850dd0a01f30ce89e1f844ad3a9215628
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-06 23:30:52 -0500

    add: L10C

--- time.cpp
+++ time.cpp
@@ -2,19 +2,37 @@
 Author: Michael Padilla
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Lab 10 B
+Assignment: Lab 10 C
 
-This program asks the users for a time and how many minutes extra will be added
-to the time and return a new moment of time
+This program prints the info of different movies after calculating its time
+slots
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+enum Genre { ACTION, COMEDY, DRAMA, ROMANCE, THRILLER };
+static const string genreStrings[] = {"ACTION", "COMEDY", "DRAMA", "ROMANCE",
+                                      "THRILLER"};
 
 class Time {
 public:
   int h;
   int m;
+};
+
+class Movie {
+public:
+  string title;
+  Genre genre;
+  int duration;
+};
+
+class TimeSlot {
+public:
+  Movie movie;
+  Time startTime;
 };
 
 // returns the number of minutes from 0:00AM until time
@@ -36,18 +54,36 @@ Time addMinutes(Time time0, int min) {
   return time;
 }
 
+// prints a timeSlot in the format: title GENRE (duration) [starts at startTime,
+// ends by Time]
+void printTimeSlot(TimeSlot ts) {
+  Time endsBy = addMinutes(ts.startTime, ts.movie.duration);
+  string line = ts.movie.title + genreStrings[ts.movie.genre];
+  line += " (" + to_string(ts.movie.duration) + ")";
+  line += " [starts at " + to_string(ts.startTime.h) + ":" +
+          to_string(ts.startTime.m);
+  line += ", end by " + to_string(endsBy.h) + ":" + to_string(endsBy.m);
+
+  cout << line << endl;
+}
+
 int main() {
-  int h, m, mins;
+  Movie movie1 = {"Back to the Future", COMEDY, 116};
+  Movie movie2 = {"Black Panther", ACTION, 134};
+  Movie movie3 = {"Kung Fu Panda 4", ACTION, 94};
+  Movie movie4 = {"Poor Things", ROMANCE, 211};
 
-  cout << "Enter time: ";
-  cin >> h >> m;
-  cout << "Enter extra minutes: ";
-  cin >> mins;
+  TimeSlot morning = {movie1, {9, 15}};
+  TimeSlot daytime = {movie2, {12, 15}};
+  TimeSlot evening = {movie2, {16, 45}};
+  TimeSlot noon = {movie3, {11, 30}};
+  TimeSlot lateNight = {movie4, {20, 00}};
 
-  Time time = {h, m};
-  time = addMinutes(time, mins);
-
-  cout << "New Time: " << time.h << ":" << time.m << endl;
+  printTimeSlot(morning);
+  printTimeSlot(daytime);
+  printTimeSlot(evening);
+  printTimeSlot(noon);
+  printTimeSlot(lateNight);
 
   return 0;
 }
