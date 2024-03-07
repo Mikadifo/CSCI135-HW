6fbd0f1c41294d3b981b8ad1153ea8661e432021
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-06 22:11:08 -0500

    add: L9F

---
+++ particle.cpp
@@ -0,0 +1,25 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 9 F
+
+This program ...
+*/
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Coord3D {
+public:
+  double x;
+  double y;
+  double z;
+};
+
+int main() {
+  double x, y, z;
+
+  return 0;
+}
