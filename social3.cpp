81cdc3f3c91880f7311f569d49282918e3d0e3bc
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-07 20:23:49 -0500

    add: Lab 11C

--- social3.cpp
+++ social3.cpp
@@ -103,31 +103,18 @@ public:
 
 int main() {
   Network nw;
-  // add three users
-  nw.addUser("mario", "Mario");
-  nw.addUser("luigi", "Luigi");
-  nw.addUser("yoshi", "Yoshi");
-
-  // make them follow each other
-  nw.follow("mario", "luigi");
-  nw.follow("mario", "yoshi");
-  nw.follow("luigi", "mario");
-  nw.follow("luigi", "yoshi");
-  nw.follow("yoshi", "mario");
-  nw.follow("yoshi", "luigi");
-
-  // add a user who does not follow others
-  nw.addUser("wario", "Wario");
-
-  // add clone users who follow @mario
-  for (int i = 2; i < 6; i++) {
-    string usrn = "mario" + to_string(i);
-    string dspn = "Mario " + to_string(i);
-    nw.addUser(usrn, dspn);
-    nw.follow(usrn, "mario");
-  }
-  // additionally, make @mario2 follow @luigi
-  nw.follow("mario2", "luigi");
-
-  nw.printDot();
+  cout << nw.addUser("mario", "Mario") << endl;
+  cout << nw.addUser("luigi", "Luigi") << endl;
+  // true (1)
+  // true (1)
+  cout << nw.addUser("mario", "Mario2") << endl;
+  // false (0)
+  cout << nw.addUser("mario 2", "Mario2")
+       << endl; // false (0) cout « nw. addUser ("mario-2",
+                // "Mario2") « endl; // false (0)
+  for (int i = 2; i < 20; i++)
+    cout << nw.addUser("mario" + to_string(i), "Mario" + to_string(i)) << endl;
+  // true (1)
+  cout << nw.addUser("yoshi", "Yoshi") << endl;
+  // false (0)
 }
