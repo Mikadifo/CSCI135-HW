64f17d5a9c73d6a215affb7f6fc785ac6857dad8
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-14 21:12:43 -0400

    P2C

--- MemoryGame.cpp
+++ MemoryGame.cpp
@@ -100,7 +100,7 @@ void MemoryGame::display() const {
   // then pad a space to its left.
   cout << endl;
   printSeparatedLine(numSlots);
-  cout << " ";
+  cout << "|";
   for (int i = 0; i < numSlots; i++) {
     if (values[i] == "") {
       cout << setw(5) << "|";
