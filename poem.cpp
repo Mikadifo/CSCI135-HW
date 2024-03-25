cbd2c84a39f08524eef6bb68786e3fe821d92dbb
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-25 09:54:53 -0400

    L9D

--- poem.cpp
+++ poem.cpp
@@ -10,18 +10,18 @@ This program keeps printing poems and storing them dinamically
 #include <iostream>
 using namespace std;
 
-string *createAPoemDynamically() {
-  string *p = new string;
-  *p = "Roses are red, violets are blue";
-  return p;
+string * createAPoemDynamically() {
+    string *p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
 }
 
 int main() {
-  while (true) {
-    string *p;
-    p = createAPoemDynamically();
+    while(true) {
+        string *p;
+        p = createAPoemDynamically();
 
-    // assume that the poem p is not needed at this point
-    delete p;
-  }
+        // assume that the poem p is not needed at this point
+	delete p;
+    }
 }
