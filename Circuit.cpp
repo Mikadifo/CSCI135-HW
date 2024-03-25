59e51bc817cedd3d53c84a9319edde436504f7d5
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-03-25 14:57:11 -0400

    E9.3

--- Circuit.cpp
+++ Circuit.cpp
@@ -6,20 +6,23 @@ Assignment: Homework E9.3
 
 Simulates a highway lapm with siwtches at both sides
 */
-#include "Circuit.hpp"
 
-int Circuit::get_first_switch_state() { return first_switch; };
+class Circuit {
+public:
+  int get_first_switch_state() { return first_switch; }
+  int get_second_switch_state() { return second_switch; }
+  int get_lamp_state() { return lamp_state; }
+  void toggle_first_switch() {
+    first_switch = !first_switch;
+    lamp_state = !lamp_state;
+  }
+  void toggle_second_switch() {
+    second_switch = !second_switch;
+    lamp_state = !lamp_state;
+  }
 
-int Circuit::get_second_switch_state() { return second_switch; };
-
-int Circuit::get_lamp_state() { return lamp_state; };
-
-void Circuit::toggle_first_switch() {
-  first_switch = !first_switch;
-  lamp_state = !lamp_state;
-};
-
-void Circuit::toggle_second_switch() {
-  second_switch = !second_switch;
-  lamp_state = !lamp_state;
+private:
+  int first_switch = 0;
+  int second_switch = 0;
+  int lamp_state = 0;
 };
