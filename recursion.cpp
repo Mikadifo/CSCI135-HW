9b6c82a8758a87ba4a9645f9994219d02d49ca45
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-08 13:05:56 -0400

    add L13B

--- recursion.cpp
+++ recursion.cpp
@@ -2,13 +2,20 @@
 Author: Michael Padilla
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Lab 13 A
+Assignment: Lab 13 B
 
-This program prints numbers in a range using recursion
+This program sums all the numbers within a given range
 */
 
 #include <iostream>
 using namespace std;
+
+int sumRange(int left, int right) {
+  if (right - left == 0)
+    return left;
+
+  return left + sumRange(left + 1, right);
+}
 
 void printRange(int left, int right) {
   if (left <= right) {
@@ -18,6 +25,11 @@ void printRange(int left, int right) {
 }
 
 int main() {
-  printRange(-2, 10);
+  int x = sumRange(1, 3);
+  cout << "This is " << x << endl; // 6
+
+  int y = sumRange(-2, 10);
+  cout << "That is " << y << endl; // 52
+
   return 0;
 }
