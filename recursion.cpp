6c6b1ef89ac5b72e386f71fc44dc98a1bdbd641b
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-08 12:48:12 -0400

    add L13A

---
+++ recursion.cpp
@@ -0,0 +1,23 @@
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 13 A
+
+This program prints numbers in a range using recursion
+*/
+
+#include <iostream>
+using namespace std;
+
+void printRange(int left, int right) {
+  if (left < right)
+    cout << left << " ";
+
+  printRange(left - 1, right);
+}
+
+int main() {
+  printRange(-2, 10);
+  return 0;
+}
