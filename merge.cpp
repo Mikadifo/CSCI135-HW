5260b1b9d6cc87839439dc96e9ecb1fe294e1989
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-13 13:13:37 -0400

    add: E6.20

--- merge.cpp
+++ merge.cpp
@@ -2,28 +2,31 @@
 Author: Michael Padilla
 Course: CSCI-135
 Instructor: Tong Yi
-Assignment: Homework E6.18
+Assignment: Homework E6.20
 
-This program appends one vector after another
+This program merges two sorted vectors
 */
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> append(vector<int> a, vector<int> b) {
+vector<int> merge_sorted(vector<int> a, vector<int> b) {
   for (int number : b)
     a.push_back(number);
+
+  sort(a.begin(), a.end());
 
   return a;
 }
 
 int main() {
   vector<int> a = {1, 4, 9, 16};
-  vector<int> b = {9, 7, 4, 9, 11};
+  vector<int> b = {4, 7, 9, 9, 11};
 
-  for (int number : append(a, b))
+  for (int number : merge_sorted(a, b))
     cout << number << endl;
 
   return 0;
