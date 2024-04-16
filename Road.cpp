d951f8ac877da8669da5b32396a2d929e80bb006
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-16 15:24:22 -0400

    add: P3C

---
+++ Road.cpp
@@ -0,0 +1,48 @@
+// Defines constructors and methods in Road.hpp
+
+/*
+Author: Michael Padilla
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Project 2C
+*/
+
+#include "Road.hpp"
+#include <iostream>
+#include <vector>
+
+// Default constructor
+Road::Road() : Road(10) {}
+
+/*
+ * @param int length
+ */
+Road::Road(int length) {
+  blocks = std::vector<char>{};
+
+  for (int i = 0; i < length; i++) {
+    blocks.push_back('_');
+  }
+}
+
+/*
+ * @param int index
+ * @param char ch
+ */
+void Road::mark(int index, char ch) { blocks.at(index) = ch; }
+
+/*
+ * Displays the current road
+ */
+void Road::display() const {
+  for (char block : blocks) {
+    std::cout << block;
+  }
+
+  std::cout << std::endl;
+}
+
+/*
+ * @return int length
+ */
+int Road::length() const { return blocks.size(); }
--- Tortoise.hpp
+++ Tortoise.hpp
@@ -12,7 +12,7 @@ public:
   int getPosition() const;
   void setPosition(int position);
 
-  // private:
+private:
   std::vector<int> patterns;
   int position;
 };
